62a5c282e6931c9812854c272ecfb33dad389694
AnhTuan2607 <tuangaming2607@gmail.com>
2024-08-27 13:18:08 +0700

    BaiTapVeNha

---
+++ BaiTapVeNha/BaiTapVeNha/Controllers/TheLoaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
